4729d28a16fdbbc6a5e04f31de7d5d922f4d996c
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-05-25 02:07:21 +0200

    fix data type

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
     def width(self, value):
         """ Setter for width
         """
-        if type(value) != int:
+        if isinstance(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -62,7 +62,7 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """ print() __str__ method funtion to return rectangle in char '#'
+        """ print() __str__ method funtion to return rectangle in char
         """
         res = ""
         if self.__width == 0 or self.__height == 0:
