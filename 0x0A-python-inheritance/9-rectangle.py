4ccc9f5a9a029b86d06552fe8a2b4fe6fc287536
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-05-30 19:40:14 +0200

    fix req fail

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""9-rectangle.py
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class that defines a rectangle from BaseGeometry Class """
+    """ Class defining rectangle from BaseGeometry Class """
 
     def __init__(self, width, height):
         """ Initializes instance """
