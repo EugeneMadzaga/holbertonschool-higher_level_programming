4ddf4370400af5614cc5ac53649f2c9b11bceedd
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-05-31 23:08:39 +0200

    push 12 files

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    """ Function that returns the pascal triangle
+    """ Function that returns the pascal triangle"""
 
-    Args:
-        n: number of lines
-
-    Returns:
-        matrix: a matrix with the pascal triangle
-
-    """
 
     matrix = []
     prev = []
