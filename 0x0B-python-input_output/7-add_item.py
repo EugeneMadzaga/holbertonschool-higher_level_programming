07f3556bc99e9abdf713bdbaeebcc2154a5b42c0
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-06-01 16:24:33 +0200

    fix 7 file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,14 @@ import os.path
 import json
 
 
-save_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+if os.path.exists("7-add_item.json"):
+    my_list = load_file("7-add_item.json")
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
-save_file(my_list, "add_item.json")
+save_file(my_list, "7-add_item.json")
