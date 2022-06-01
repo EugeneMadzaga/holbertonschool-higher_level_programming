ffca4db22f194a485b7f4b01eaad61fc3b4ddde7
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-06-01 19:22:18 +0200

    quick fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    """Function that returns the pascal triangle
-    """
+"""Function that returns the pascal triangle
+"""
 
     matrix = []
     prev = []
