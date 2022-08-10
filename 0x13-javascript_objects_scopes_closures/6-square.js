90f845f07dd9c5784957fc8f6130e6449bf62929
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-08-10 09:03:32 +0100

    fix 6th file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
+const Square1 = require('./4-rectangle.js');
+
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
     }
   }
 };
